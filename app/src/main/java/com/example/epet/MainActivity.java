fcc2fd4daab3cdf4f0109d3656789af31b5adfd2
ticinha <ticiafmenezes@gmail.com>
2024-08-13 21:51:42 -0300

    adicionei telas novas.

--- app/src/main/java/com/example/epet/MainActivity.java
+++ app/src/main/java/com/example/epet/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.epet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //criando variável dos botões
+        ImageButton imgbDenun = findViewById(R.id.imgbDenun);
+        ImageButton imgbAdocao  = findViewById(R.id.imgbAdocao);
+
+        //evento ao clicar no botão
+        imgbDenun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //método para abrir a outra tela
+                Intent intent = new Intent(MainActivity.this, telaDenuncia.class);
+                startActivity(intent);
+            }
+        });
     }
 }
