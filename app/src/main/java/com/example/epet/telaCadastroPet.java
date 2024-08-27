686ba9ede364b9866e00330cd2c3af044cb7282e
gabi <gabrielliv31@gmail.com>
2024-08-26 21:41:59 -0300

    Início do design da tela de cadastro do pet.

---
+++ app/src/main/java/com/example/epet/telaCadastroPet.java
@@ -0,0 +1,14 @@
+package com.example.epet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaCadastroPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_cadastro_pet);
+    }
+}
