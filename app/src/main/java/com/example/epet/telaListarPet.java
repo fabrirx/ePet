a89ace251a1b6f19582bba9a35bc3b5145a4b248
gabi <gabrielliv31@gmail.com>
2024-09-04 21:28:54 -0300

    Concluído o design da tela de listar o pet do administrador.

---
+++ app/src/main/java/com/example/epet/telaListarPet.java
@@ -0,0 +1,14 @@
+package com.example.epet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class telaListarPet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_listar_pet);
+    }
+}
