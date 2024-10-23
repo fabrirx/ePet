d9450cf1d9a661a9b5507ab920f7a6fff03fa034
gabi <gabrielliv31@gmail.com>
2024-10-22 21:20:54 -0300

    Concluído o design da tela de denúncia de abandono.

--- app/src/main/java/com/example/epet/telaDenunciaAbandono.java
+++ app/src/main/java/com/example/epet/telaDenunciaAbandono.java
@@ -2,7 +2,9 @@ package com.example.epet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 
 public class telaDenunciaAbandono extends AppCompatActivity {
 
