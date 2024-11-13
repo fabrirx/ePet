aae1c89bac3c50d67e3badb51985f917429d21c4
fabri <fabrigabriel321@gmail.com>
2024-11-12 21:41:08 -0300

    Ligar Para IBAMA Funcionado :D

--- app/src/main/java/com/example/epet/telaDenunciaMausTratos.java
+++ app/src/main/java/com/example/epet/telaDenunciaMausTratos.java
@@ -2,7 +2,11 @@ package com.example.epet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class telaDenunciaMausTratos extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class telaDenunciaMausTratos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_denuncia_maus_tratos);
+
+
+        ImageButton ligar = findViewById(R.id.btLigar);
+
+
+        ligar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fazerChamada();  // Chama o método que faz a ligação
+            }
+        });
+    }
+
+    // Método para realizar a chamada
+    public void fazerChamada() {
+        String numeroTelefone = "tel:0800618080";
+
+        // Cria o Intent para abrir a interface de discagem com o número
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(numeroTelefone));
+
+        // Verifica se existe algum app de telefone no dispositivo
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);  // Inicia o Intent para abrir a interface de chamadas
+        }
     }
 }
