4f21d365339c80e16bdc10622506f256d5f6b66c
Gabi <gabrielliv31@gmail.com>
2024-11-12 21:26:59 -0300

    Começo do design da tela apadrinhar.

--- app/src/main/java/com/example/epet/telaApadrinhar.java
+++ app/src/main/java/com/example/epet/telaApadrinhar.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class telaGerenciamento extends AppCompatActivity {
+public class telaApadrinhar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tela_gerenciamento);
+        setContentView(R.layout.activity_tela_apadrinhar);
     }
 }
